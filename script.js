1ec08f3aaab227eb096991506677d8f116232b8a
bakari-rhoka-bubu <bakaribubu@gmail.com>
2024-06-23 12:03:29 +0300

    Adds html,css and script files

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.addEventListener("DOMContentLoaded", function() {
+    const aboutLink = document.getElementById("about-link");
+    const skillsLink = document.getElementById("skills-link");
+    const interestLink = document.getElementById("interest-link");
+
+    const aboutSection = document.getElementById("about");
+    const skillsSection = document.getElementById("skills");
+    const interestSection = document.getElementById("interest");
+
+    aboutLink.addEventListener("click", function() {
+        aboutSection.style.display = "block";
+        skillsSection.style.display = "none";
+        interestSection.style.display = "none";
+    });
+
+    skillsLink.addEventListener("click", function() {
+        aboutSection.style.display = "none";
+        skillsSection.style.display = "block";
+        interestSection.style.display = "none";
+    });
+
+    interestLink.addEventListener("click", function() {
+        aboutSection.style.display = "none";
+        skillsSection.style.display = "none";
+        interestSection.style.display = "block";
+    });
+});
